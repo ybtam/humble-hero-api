2bddd05dc9ddf3085f63ac1d7477ba3ee8ee0e1e
Yi Tam <tam.yi3d@gmail.com>
2025-02-09 17:11:38 +0100

    feat: create schema

---
+++ apps/api/src/hero/schema.ts
@@ -0,0 +1,16 @@
+import { sql } from 'drizzle-orm'
+import { check, int, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+
+export const heros = sqliteTable(
+  'heros',
+  {
+    humilityScore: int('humility_score'),
+    id: int('id').primaryKey({ autoIncrement: true }),
+    name: text('name'),
+    superpowers: text('superpowers'),
+  },
+  table => [
+    check('humility_score_min', sql`${table.humilityScore} >= 0`),
+    check('humility_score_max', sql`${table.humilityScore} <= 10`),
+  ],
+)
